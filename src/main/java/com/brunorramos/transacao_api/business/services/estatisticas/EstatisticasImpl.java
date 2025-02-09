66a4ae994b643aca9bf0774c7c50a77776541ce0
BrunoRamos <bruno.ramos@ccc.ufcg.edu.br>
2025-02-09 12:17:52 -0300

    chore: Adicionando logs nos services de TransacaoImpl e EstatisticasImpl

--- src/main/java/com/brunorramos/transacao_api/business/services/estatisticas/EstatisticasImpl.java
+++ src/main/java/com/brunorramos/transacao_api/business/services/estatisticas/EstatisticasImpl.java
@@ -4,6 +4,7 @@ import com.brunorramos.transacao_api.business.services.transacao.TransacaoServic
 import com.brunorramos.transacao_api.controller.dtos.EstatisticasResponseDTO;
 import com.brunorramos.transacao_api.controller.dtos.TransacaoDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.DoubleSummaryStatistics;
@@ -11,15 +12,19 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EstatisticasImpl implements EstatisticasService {
 
     private final TransacaoService transacaoService;
 
     public EstatisticasResponseDTO calcularEstatisticas(Integer intervalo) {
+        log.info("Calculando estatisticas para intervalo de {} segundos", intervalo);
+
         List<TransacaoDTO> transacoes = transacaoService.buscarTransacoes(intervalo);
         DoubleSummaryStatistics estatisticasDasTransacoes = transacoes.stream()
                 .mapToDouble(TransacaoDTO::valor).summaryStatistics();
 
+        log.info("Retornando estatisticas com sucesso !");
         return new EstatisticasResponseDTO(
                 estatisticasDasTransacoes.getCount(),
                 estatisticasDasTransacoes.getSum(),
--- src/main/java/com/brunorramos/transacao_api/business/services/transacao/TransacaoImpl.java
+++ src/main/java/com/brunorramos/transacao_api/business/services/transacao/TransacaoImpl.java
@@ -20,21 +20,26 @@ public class TransacaoImpl implements TransacaoService {
 
     @Override
     public void createTransacao(TransacaoDTO transacaoDTO) throws UnprocessableEntity {
-        log.info("Iniciando criação de nova transacao");
+        log.info("Iniciando criação de nova transacao: {}", transacaoDTO);
         Validators.validaNulos(transacaoDTO);
         Validators.validaValor(transacaoDTO.valor());
         this.transacaoRepository.create(transacaoDTO);
+        log.info("Transação criada com sucesso !");
     }
 
     @Override
     public List<TransacaoDTO> buscarTransacoes(Integer intervalo) {
+        log.info("Iniciando a busca de transações referentes aos últimos {} segundos", intervalo);
         OffsetDateTime dataHoraIntervalor = OffsetDateTime.now().minusSeconds(intervalo);
         List<TransacaoDTO> registros = this.transacaoRepository.buscarRegistros(dataHoraIntervalor);
+        log.info("Retornando transações com sucesso !");
         return registros;
     }
 
     @Override
     public void delete() {
+        log.info("Iniciando deleção das transações ...");
         this.transacaoRepository.delete();
+        log.info("Todas as transações foram apagadas com sucesso !");
     }
 }
