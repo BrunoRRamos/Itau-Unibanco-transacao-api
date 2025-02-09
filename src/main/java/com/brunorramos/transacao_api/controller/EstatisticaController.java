3346f2f4b6ae7ade63a490661adc2ce28bb1af06
BrunoRamos <bruno.ramos@ccc.ufcg.edu.br>
2025-02-09 18:55:34 -0300

    doc: Documentação do endpoint de /estatistica

--- src/main/java/com/brunorramos/transacao_api/controller/EstatisticaController.java
+++ src/main/java/com/brunorramos/transacao_api/controller/EstatisticaController.java
@@ -1,8 +1,10 @@
 package com.brunorramos.transacao_api.controller;
 
 import com.brunorramos.transacao_api.business.services.estatisticas.EstatisticasService;
-import com.brunorramos.transacao_api.business.services.transacao.TransacaoService;
 import com.brunorramos.transacao_api.controller.dtos.EstatisticasResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,12 @@ public class EstatisticaController {
     private final EstatisticasService estatisticasService;
 
     @GetMapping
+    @Operation(description = "Este endpoint é responsável por criar uma estatisca das ultimas transações em um intervalo de tempo")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Estatísticas geradas com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<EstatisticasResponseDTO> gerarEstatisticas(
             @RequestParam(value = "intervalo de busca", required = false, defaultValue = "60") Integer intervaloDeBusca) {
         EstatisticasResponseDTO estatistica = this.estatisticasService.calcularEstatisticas(intervaloDeBusca);
--- src/main/java/com/brunorramos/transacao_api/controller/TransacaoController.java
+++ src/main/java/com/brunorramos/transacao_api/controller/TransacaoController.java
@@ -2,6 +2,9 @@ package com.brunorramos.transacao_api.controller;
 
 import com.brunorramos.transacao_api.business.services.transacao.TransacaoService;
 import com.brunorramos.transacao_api.controller.dtos.TransacaoDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +18,25 @@ public class TransacaoController {
     private final TransacaoService transacaoService;
 
     @PostMapping
+    @Operation(description = "Este endpoint é responsável por criar uma nova Transação")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transação gravada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Os campos não atendem aos requisitos da transação"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<Void> adicionarTransacao(@RequestBody TransacaoDTO transacaoDTO) {
         this.transacaoService.createTransacao(transacaoDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping
+    @Operation(description = "Este endpoint é responsável por deletar todas as Transações")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Transações deletadas com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<Void> limparTransacoes() {
         this.transacaoService.delete();
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
